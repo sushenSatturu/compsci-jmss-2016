d6101dc14d9c9c816bed7922828722b95fa281ca
Sushen <sat0006@jmss.vic.edu.au>
2016-02-08 14:51:53 +1100

    Added an exception

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,18 @@
 my_list = [1,2,3,5]
+'''
+try:
+    x = my_list[5]
+    print(x)
+except IndexError:
+    print("Out of Range")
+'''
 
-#try:
-x = my_list[5]
-#except IndexError:
-#    print("Out of Range")
+notNum = True
+while notNum:
+    try:
+        x = input("Please enter a number ")
+        x = int(x)
+        notNum = False
+    except ValueError:
+        print("Plz enter a integer numeric, i.e 1, 2, 3, 4 etc")
+
