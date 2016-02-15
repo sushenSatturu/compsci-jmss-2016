63f1df32c7f7f5b4c93c6d77042e81ce54200a0b
lindamciver <linda.mciver@jmss.vic.edu.au>
2016-02-15 11:38:22 +1100

    string exercises with for loops

---
+++ stringystuff.py
@@ -0,0 +1,23 @@
+
+
+'''
+1. String concatenation:
+        	x=”cat”
+        	y=”dog”
+
+        	What does x+y equal?
+
+
+Write a for loop that takes a list of words
+words=[“cat”,”dog”,”fish”,”sugar glider”,”frog”,”rabbit”]
+makes it into one string and prints out that string.
+'''
+
+words = ["cat","dog","fish","sugar glider","frog","rabbit"]
+
+for word in words:
+
+# 2. Now INSIDE the for loop, search for a particular word and change it in the
+# final string. Eg search for "cat" and change it to "dog" before you create the string.
+
+# 3. Change your for loop to use the index instead of the items.
