76f2b8cfb4a0fd19aeb1faec7a494579563249c6
lindamciver <linda.mciver@jmss.vic.edu.au>
2016-03-04 12:57:12 +1100

    string slicing and multiplying examples

---
+++ stringSlicing.py
@@ -0,0 +1,17 @@
+#accessing parts of strings
+
+name = "Dr Linda Mciver"
+
+#print (name[0:15:3])
+
+print(name*2)
+
+sentence = ["the","problem","is","or","rather","one","of","the","problems"]
+
+print (sentence*2)
+
+#change the following code to print out an isoceles triangle properly (so no vertical edges!)
+
+for i in range(10):
+    print("x"*i)
+
